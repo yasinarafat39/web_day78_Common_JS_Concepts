fd9e9079ba04687d1334e7fae7ae46fbee5625b1
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-28 13:26:06 +0600

    == vs ===

---
+++ triple.js
@@ -0,0 +1,16 @@
+
+// always use ===
+// equal comparison doesn't work for Non-Primitive types
+
+const first = 2;
+const second = '2';
+
+if(first === second){
+    console.log('values are equal');
+} else{
+    console.log('values are not equal');
+}
+
+// type coercion
+// type conversion
+// type casting
