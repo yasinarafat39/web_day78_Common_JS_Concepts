2e2c5b1d4d92e6fa4253da8e0c205b94875442cc
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-28 12:29:47 +0600

    Primitive Data type and Non-Primitive Data types

---
+++ types.js
@@ -0,0 +1,22 @@
+// Javascript is a dynamic type languages.
+
+// Primitive Type
+const a = 5; // number
+const b = "Yasin Arafat"; // string
+const c = true; // boolean
+
+// Non-Primitive Type
+const ages = [45, 55, 65, 14];
+const student = {id: 23, class: 4}
+
+
+let x = 5;
+let y = x;
+y = 7;
+console.log(x, y);
+
+let p = {job: 'Web Development'};
+let q = p;
+p.job = "front end development"
+// q = {job: 'Backend end'}
+console.log(p, q);
