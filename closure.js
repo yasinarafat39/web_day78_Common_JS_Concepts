ed97bc45184031638b52b36f71da6dfd5bda5aae
Yasin Arafat <yasinarafat2557@gmail.com>
2024-09-28 14:16:00 +0600

    Closure

---
+++ closure.js
@@ -0,0 +1,16 @@
+
+
+
+function kitchen(){
+    let roast = 0;
+    return function(){
+        roast ++;
+        return roast;
+    }
+}
+
+const firstServer = kitchen();
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
